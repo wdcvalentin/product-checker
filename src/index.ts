d666215d4034d922acc29c6c8c8a9a0f27b4d5a1
Valentin <vwachedecorbie@sarenza.com>
2021-02-13 17:15:15 +0100

    (unfixed) handling error

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,33 @@ import axios from 'axios';
 import cheerio from 'cheerio';
 
 const AxiosInstance = axios.create();
-const productIds = {
-  id_1: 'PB00385045',
-  id_2: 'PB00254175',
-  // id_3: 'PB99993070'
-}
-const getData = async (url: string) => {
-  const response = await AxiosInstance.get(url);
-  const html = response.data;
-  const $ = cheerio.load(html);
-  if (!$('.title > h1').text()) {
-    return {
-      productName : null,
-      availability : null,
-      price : null,
+const productIds = [
+  'PB00385045',
+  'PB00254175',
+  // 👇👇 is an error test
+  // 'PB99993070'
+]
+
+const logging = async (url: string, id: string) => {
+  const { productName, availability, price } = await getData(url, id);
+  console.log('price', price);
+  console.log('productName', productName);
+  console.log('availability : ', availability !== 'Rupture' ? true : false);
+  console.log('------------------------------------------------------------');
+};
+
+const getData = async (url: string, id: string) => {
+  try {
+    const response = await AxiosInstance.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    if (!$('.title > h1').text()) {
+      return {
+        productName: null,
+        availability: null,
+        price: null,
+      }
     }
-  } else {
     const productName = $('.title > h1').text();
     const availability = $('.content > div > span').text();
     const price = $('.product-info > .wrap-aside > aside > .price >.price').text();
@@ -26,19 +37,19 @@ const getData = async (url: string) => {
       availability,
       price
     }
-  }
-}
-for (let id in productIds) {
-  const url = `https://www.ldlc.com/fiche/${productIds[id]}.html`;
-  getData(url)
-  const logging = async (url: string) => {
-    const { productName, availability, price } = await getData(url);
-    console.log('price', price)
-    console.log('productName', productName)
-    console.log('availability : ', availability !== 'Rupture' ? true : false);
-    console.log('------------------------------------------------------------')
-  }
+  } catch (error) {
+    productIds.filter(p_id => p_id !== id);
+    productIds.some(_id => _id === id)
+    console.log(`${url} is down`);
+  };
+};
+
+productIds.map(id => {
+  const url: string = `https://www.ldlc.com/fiche/${id}.html`;
+  getData(url, id);
+  // logging(url);
+
   setInterval(() => {
-    logging(url);
+    logging(url, id);
   }, 3000);
-}
+})
